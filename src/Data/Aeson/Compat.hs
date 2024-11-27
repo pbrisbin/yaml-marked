ba328112c67ac9503ee67dbc24131184ec50dfdb
patrick brisbin <pbrisbin@gmail.com>
2024-11-27 13:13:46 -0500

    Fix aeson CPP, yikes

--- src/Data/Aeson/Compat.hs
+++ src/Data/Aeson/Compat.hs
@@ -3,16 +3,32 @@
 module Data.Aeson.Compat
 #if MIN_VERSION_aeson(2, 0, 0)
   ( module Data.Aeson
+  , parseEither
   ) where
 
 import Data.Aeson
+
+#if MIN_VERSION_aeson(2, 1, 0)
+import Prelude
+import Data.Aeson.Types (Parser, IResult(..), iparse)
+
+parseEither :: (a -> Parser b) -> a -> Either String b
+parseEither f a = case iparse f a of
+  IError _ x -> Left x
+  ISuccess b -> Right b
+#else
+import Data.Aeson.Types (parseEither)
+#endif
+
 #else
   ( Key
   , module Data.Aeson
+  , parseEither
   ) where
 
 import Data.Aeson
 import Data.Text (Text)
+import Data.Aeson.Types (parseEither)
 
 type Key = Text
 #endif
