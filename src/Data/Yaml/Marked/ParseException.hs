7584ba4851d44358dbb93e60722afad77ace7ecb
patrick brisbin <pbrisbin@gmail.com>
2024-11-27 10:43:24 -0500

    WIP: displayParseException

---
+++ src/Data/Yaml/Marked/ParseException.hs
@@ -0,0 +1,28 @@
+module Data.Yaml.Marked.ParseException
+  ( ParseException (..)
+  , YamlException (..)
+  , displayParseException
+  ) where
+
+import Prelude
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS8
+import Data.Yaml (ParseException (..), prettyPrintParseException)
+import Text.Libyaml (YamlException (..))
+
+displayParseException :: Maybe ByteString -> ParseException -> String
+displayParseException = \case
+  Nothing -> prettyPrintParseException
+  Just bs -> prettyPrintParseExceptionInSource bs
+
+prettyPrintParseExceptionInSource :: ByteString -> ParseException -> String
+prettyPrintParseExceptionInSource bs = \case
+  InvalidYaml (Just ex) -> undefined
+  ex ->
+    unlines
+      [ prettyPrintParseException ex
+      , "Input:"
+      , BS8.unpack $ BS8.unlines $ map ("  | " <>) $ BS8.lines bs
+      , ""
+      ]
